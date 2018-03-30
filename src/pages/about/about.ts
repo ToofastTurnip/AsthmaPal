af3d95bbdb2ca3733f99771fc954a081c5fcbc37
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-03-30 08:33:28 -0400

    Range experimentation

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -3,9 +3,14 @@ import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
-  templateUrl: 'about.html'
+  templateUrl: 'about.html',
 })
 export class AboutPage {
+
+  knobValues:any = { lower: 100, upper: 2300 }
+  public event = {
+    timeStarts: '08:00'
+  }
 
   constructor(public navCtrl: NavController) {
 
