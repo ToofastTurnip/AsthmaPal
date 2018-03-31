4a467a2d07e512d05c8f98576f8effc45b9e66fd
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-03-31 17:14:09 -0400

    Reset data button progress (not done)

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
@@ -12,8 +14,23 @@ export class AboutPage {
     timeStarts: '08:00'
   }
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private storage: Storage, public toastCtrl: ToastController) {
 
+  }
+
+  resetData() {
+    let toast = this.toastCtrl.create({
+      message: 'Are you sure?',
+      duration: 3000,
+      showCloseButton: true,
+      closeButtonText: 'Yep'
+    });
+    toast.onDidDismiss((data, role) => {
+      if (role == 'close') {
+        this.storage.clear();
+      }
+    });
+    
   }
 
 }
